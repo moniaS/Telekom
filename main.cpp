c3df25288b2eebb438ee835226856bc0ad7112e8
Monika <monika.stobieniecka1@wp.pl>
2016-04-03 17:32:51 +0200

    Poprawa kodu

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <math.h>
 #include <locale.h>
+#include <algorithm>
 
 //#define UNICODE
 
@@ -14,7 +15,7 @@ const int wiersze = 8; //m w sprawozdaniu
 const int bitowNaSlowo = 16; //m+n w sprawozdaniu
 const char* PLIK_WYSYLANA = "wysylana.txt";
 const char* PLIK_ODEBRANA = "odebrana.txt";
-const int PRZESUNIECIE = 3; //W kodowaniu pliku Windows-1250 musimy pominąć pierwsze 3 znaki.
+const int PRZESUNIECIE = 0; //W kodowaniu pliku Windows-1250 musimy pominąć pierwsze 3 znaki.
 
 bool H[wiersze][bitowNaSlowo] = {
                 {1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0},
@@ -92,136 +93,99 @@ void kodowanie(char znak)
     }
 }
 
-
-int naASCII()
-{
-    double kodASCII = 0.0;
-
-    for(int i = 0, j = 7; i < 8; i++, j--)
-        kodASCII += (R[i] * pow(2.0,j));
-
-    return (int)kodASCII;
-}
-
-
-bool czyBlad()
+bool czyDecodeMaBlad()
 {
     for(int i = 0; i < wiersze; i++)
-        if(Decode[i] != 0) return true;
-
+    {
+        if(Decode[i] != 0)
+            return true;
+    }
     return false;
 }
 
-
-int szukaj1Blad() throw(string)
-{
-    bool found;
-    string e = "  Wiecej niz 1 blad, szukam dalej!";
-
-
-    for(int i = 0; i < bitowNaSlowo; i++)
-    {
-        found = true;
-        for(int j = 0; j < wiersze; j++)
-        {
-            if(H[j][i] != Decode[j])
-            {
-                found = false;
-                break;
-            }
-        }
-        if(found) return i; //zwraca pozycje, na ktorej znajduje sie blad
+//porownujemy z wybrana kolumna macierzy H, szukając bledu
+bool czyDecodeRownyKolumnie(int nrKolumny) {
+    for (int i = 0; i < wiersze; i++) {
+        if (Decode[i] != H[i][nrKolumny])
+            return false;
     }
-    if(!found) throw e;
+    return true;
 }
 
+bool czyDecodeRownySumieKolumn (int nrKolumny1, int nrKolumny2) {
+    for (int i = 0; i < wiersze; i++) {
+        int sumaKolumn = H[i][nrKolumny1] + H[i][nrKolumny2];
+        sumaKolumn %= 2;
+        if (Decode[i] != sumaKolumn)
+            return false;
+    }
+    return true;
+}
 
-int *szukaj2Bledy() throw(string)
-{
-    bool found;
-    int *k = new int[2];
-    string e = "  Wiecej niz 2 bledy!";
+int ktorejKolumnieRownyDecode() {
+    for (int wiersz = 0; wiersz < bitowNaSlowo; wiersz++) {
+        if (czyDecodeRownyKolumnie(wiersz))
+            return wiersz;
+    }
+    return -1; //jesli zaden nie jest rowny, to zwroc -1
+}
 
-    for(int i = 0; i < bitowNaSlowo-1; i++)
-    {
-        for(int l = i+1; l < bitowNaSlowo; l++)
-        {
-            found = true;
-
-            for(int j = 0; j < wiersze; j++)
-            {
-                if(((H[j][i] + H[j][l])%2) != Decode[j])
-                {
-                    found = false;
-                    break;
-                }
-            }
-            if(found)
-            {
-                k[0] = i;
-                k[1] = l;
-                return k;
+int* ktorymDwomKolumnomRownyDecode() {
+    for (int i = 0; i < bitowNaSlowo - 1; i++) {
+        for (int j = i + 1; j < bitowNaSlowo; j++) {
+            if (czyDecodeRownySumieKolumn(i, j)) {
+                return new int[2]{i,j};
             }
         }
     }
-    if(!found) throw e;
+    return NULL; //jesli zadna para nie jest rowna, to zwroc nulla
 }
-
 
 void naprawBit(int pozycjaBledu)
 {
-    R[pozycjaBledu] += 1;
+    R[pozycjaBledu] ++;
     R[pozycjaBledu] %= 2;
 }
 
-
-void napraw2Bledy()
-{
-    try
-    {
-        int *pozycjeBledow = new int[2];
-        pozycjeBledow = szukaj2Bledy();
-
-
-        //NAPRAWIANIE
-        naprawBit(pozycjeBledow[0]);
-        naprawBit(pozycjeBledow[1]);
-        cout << "  Bledy na bitach o pozycjach " << pozycjeBledow[0] << " i " << pozycjeBledow[1] << ".";
+char parsujRnaAscii() {
+    int mnoznik = 1;
+    int ascii = 0;
+    for (int i = wiersze-1; i >= 0; i--) {
+        ascii += R[i] * mnoznik;
+        mnoznik *= 2;
     }
-    catch(string e)
-    {
-        cout << e;
-    }
+    return (char) ascii;
 }
 
-
-void napraw1Blad()
-{
-    try
-    {
-        int pozycjaBledu;
-        pozycjaBledu = szukaj1Blad();
-
-        //NAPRAWIANIE
+void naprawZnakIWypiszBledy() {
+    int pozycjaBledu = ktorejKolumnieRownyDecode();
+    if (pozycjaBledu != -1) {
         naprawBit(pozycjaBledu);
-        cout << "  Blad na bicie o pozycji " << pozycjaBledu << ".";
-    }
-    catch(string e)
-    {
-        napraw2Bledy();
+        cout<<" ("<<pozycjaBledu<<") ";
+    } else {
+        int* pozycjeBledow = ktorymDwomKolumnomRownyDecode();
+        if (pozycjeBledow != NULL) {
+            naprawBit(pozycjeBledow[0]);
+            naprawBit(pozycjeBledow[1]);
+            cout<<" ("<<pozycjeBledow[0]<<","<<pozycjeBledow[1]<<") ";
+        } else {
+            cout<<"3 bledy :((";
+        }
     }
 }
 
-
-char dekodowanie()
+char dekodujWektorR()
 {
     for(int i = 0; i < wiersze; i++)
         Decode[i] = 0;
+
     mnozenie(R, Decode);
 
-    if(czyBlad()) napraw1Blad();
-    int kodZnaku = naASCII();
-    return char(kodZnaku);
+    if(czyDecodeMaBlad())
+    {
+        naprawZnakIWypiszBledy();
+    }
+    return parsujRnaAscii();
 }
 
 string odczytajPlik()
@@ -233,7 +197,6 @@ string odczytajPlik()
     plik_in.close();
     return wiadomosc;
 }
-
 //Pobiera bity z tablicy T w ktorej jest akrualnie wysylane slowo
 void wypiszBityDoPlikuIkonsoli(ofstream &plik_out) {
     for(int i = 0; i < bitowNaSlowo; i++)
@@ -246,7 +209,6 @@ void wypiszBityDoPlikuIkonsoli(ofstream &plik_out) {
     plik_out << "\n";
     cout << endl;
 }
-
 void zakodujIWypisz(string wiadomosc) {
     ofstream plik_out;
     plik_out.open(PLIK_ODEBRANA);
@@ -262,11 +224,26 @@ void zakodujIWypisz(string wiadomosc) {
 void parsujIWyswietl(string line) {
     for(int i = 0; i <bitowNaSlowo; i++)
     {
-        R[i] = line[i]-'0';
+        R[i] = line[i];
         cout << R[i];
         if(i == 7)
             cout << " ";
     }
+}
+
+void dekodujPlik() {
+    ifstream plik_in;
+    plik_in.open(PLIK_ODEBRANA);
+    cout << "Odebrano: " << endl;
+    string linia;
+    while (getline(plik_in, linia))
+    {
+        parsujIWyswietl(linia);
+        cout << " : " << dekodujWektorR() << endl;
+    }
+
+    plik_in.close();
+    cout << endl;
 }
 
 int main()
@@ -274,36 +251,11 @@ int main()
 	setlocale(LC_ALL,"polish"); //wyświetlanie polskich znaków
     string wiadomosc = odczytajPlik();
     zakodujIWypisz(wiadomosc);
-
     system("PAUSE");
+
     cout << endl;
-
-    char znak;
-    ifstream plik_in;
-    plik_in.open("odebrana.txt");
-
-    cout << "Odebrano: " << endl;
-    for(int i = 0; i < wiadomosc.length()-PRZESUNIECIE; i++)
-    {
-        string line;
-        getline(plik_in,line);
-        parsujIWyswietl(line);
-
-        for(int l = 0; l < wiersze; l++)
-            Decode[l] = 0;
-        cout << " : " << dekodowanie() << endl;
-    }
-
-    plik_in.close();
-    cout << endl;
-
-
+    dekodujPlik();
     system("PAUSE");
+
     return 0;
 }
-
-
-
-
-
-
